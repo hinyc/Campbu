d902eb04ff352706c047c01c913488b452403884
bgyoon <gye04056@gmail.com>
2022-01-03 17:51:20 +0900

    [Feat] #15 Footer 추가
    Menu, Profile svg 파일 추가
    로그인 버튼 클릭 시 로그인 한 버튼으로 바뀌도록 함
    푸터 정보 삽입 및 가운데 정렬

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,80 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from '@emotion/react';
+import Logo from '../assets/Logo.svg';
+
+const footerStyle = css`
+  height: 208px;
+  background-color: #eeefcb;
+`;
+
+const divStyle = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const textStyle = css`
+  font-size: 0.625rem;
+`;
+
+const ulStyle = css`
+  list-style: none;
+  padding: 0;
+`;
+
+const titleStyle = css`
+  font-size: 0.75rem;
+  font-weight: 700;
+`;
+
+const listStyle = css`
+  font-weight: 400;
+`;
+
+const lineStyle = css`
+  border: 1px solid black;
+`;
+
+const underStyle = css`
+  display: block;
+  text-align: center;
+`;
+
+function Footer() {
+  return (
+    <footer css={footerStyle}>
+      <div css={divStyle}>
+        <div>
+          <img src={Logo} className="CampBu-logo" alt="logo" />
+          <div css={textStyle}>서울특별시 강남구 논현동 384-2</div>
+        </div>
+        <ul css={ulStyle}>
+          <li css={titleStyle}>서비스 소개</li>
+          <li css={[titleStyle, listStyle]}>Wiki</li>
+          <li css={[titleStyle, listStyle]}>Github</li>
+        </ul>
+        <ul css={ulStyle}>
+          <li css={titleStyle}>Contact</li>
+          <li css={[titleStyle, listStyle]}>
+            <a href="https://github.com/Ohney">류 현</a>
+          </li>
+          <li css={[titleStyle, listStyle]}>
+            <a href="https://github.com/YeChan8812">곽예찬</a>
+          </li>
+          <li css={[titleStyle, listStyle]}>
+            <a href="https://github.com/bgyoons">백윤서</a>
+          </li>
+          <li css={[titleStyle, listStyle]}>
+            <a href="https://github.com/hinyc">홍인열</a>
+          </li>
+        </ul>
+      </div>
+      <div css={[divStyle, underStyle]}>
+        <div css={lineStyle} />
+        <p css={textStyle}>© 2021 Copyright CampBu Inc. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
