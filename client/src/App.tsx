b536fd307ad76340462d618ab2166fad7179d8dd
bgyoon <gye04056@gmail.com>
2022-01-03 17:55:42 +0900

    [Feat] #8 메인페이지 뼈대 추가
    카테고리, 검색 추가 및 간단한 스타일 적용
    상품 리스트 컴포넌트 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Footer from './components/Footer';
+// import Intro from './Intro';
+import Main from './Main';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      {/* <Intro /> */}
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ client/src/Main.tsx
@@ -0,0 +1,63 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from '@emotion/react';
+import Product from './components/Product';
+
+const divStyle = css`
+  text-align: center;
+  margin: 0 12.75rem;
+`;
+
+const ulStyle = css`
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  padding: 0;
+`;
+
+const categoryStyle = css`
+  background-color: #eeefcb;
+  margin-left: 5px;
+  margin-right: 5px;
+  width: 97px;
+  height: 122px;
+  font-size: 0.75rem;
+  font-weight: 700;
+  text-align: center;
+`;
+
+const inputStyle = css`
+  font-size: 0.875rem;
+  width: 420px;
+  height: 43px;
+`;
+
+const section = css`
+  display: flex;
+`;
+
+function Main() {
+  return (
+    <div css={divStyle}>
+      <ul css={ulStyle}>
+        <li css={categoryStyle}>전체</li>
+        <li css={categoryStyle}>패키지</li>
+        <li css={categoryStyle}>텐트/침낭</li>
+        <li css={categoryStyle}>그릴/버너</li>
+        <li css={categoryStyle}>의자/테이블</li>
+        <li css={categoryStyle}>배낭/아이스박스</li>
+        <li css={categoryStyle}>취식용품</li>
+        <li css={categoryStyle}>기타</li>
+      </ul>
+      <input placeholder="지역을 검색해보세요!" css={inputStyle} />
+      <button>검색</button>
+      <section css={section}>
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </section>
+    </div>
+  );
+}
+
+export default Main;
