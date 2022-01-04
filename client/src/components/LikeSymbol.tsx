7d697181f395c4af5266469807b8dfdec7561ea6
try <dlsduf@gmail.com>
2022-01-04 16:52:53 +0900

    [Feat] #11 찜하기 컴포넌트 생성 - 재사용성을 위하여 찜하기 버튼 컴포넌트 생성

--- client/src/components/LikeSymbol.tsx
+++ client/src/components/LikeSymbol.tsx
@@ -1,11 +1,23 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { color, rem, shadow, hover } from '../common';
+import { color, rem } from '../common';
+const like = css`
+  color: ${color.point};
+  font-size: ${rem(18)};
+  width: ${rem(83)};
+  height: ${rem(43.74)};
+  border: 1px solid ${color.border};
+  border-radius: ${rem(22)};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-const LikeCount: number = 24;
 function LikeSymbol() {
+  const LikeCount: number = 24;
+
   return (
-    <div>
+    <div css={like}>
       <div>{`♡ ${LikeCount}`}</div>
     </div>
   );
