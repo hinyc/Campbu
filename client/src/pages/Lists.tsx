b263c804b50b2ebbce98e0156f7e58e1d36e2f1f
bgyoon <gye04056@gmail.com>
2022-01-05 13:22:44 +0900

    [Style] #8 목록 탭 바 스타일 설정
    빌린 목록, 빌려준 목록 등의 탭 바 기본, hover 스타일 설정

--- client/src/pages/Lists.tsx
+++ client/src/pages/Lists.tsx
@@ -3,25 +3,56 @@ import BorrowList from './Lists/BorrowList';
 import LendList from './Lists/LendList';
 import ResistList from './Lists/ResistList';
 import LikeList from './Lists/LikeList';
+import BackButton from '../components/BackButton';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { rem, color, flex } from '../common';
+
+const container = css`
+  width: ${rem(1280)};
+  margin: 0 auto;
+  height: ${rem(50)};
+  align-items: center;
+`;
+
+const link = css`
+  color: ${color.placeholder};
+  text-decoration: none;
+  margin-left: ${rem(16)};
+  margin-right: ${rem(41)};
+  font-weight: 400;
+  font-size: ${rem(16)};
+  padding: 0 0 ${rem(6)} 0;
+
+  &:hover {
+    color: ${color.deep};
+    font-weight: 700;
+    border-bottom: 2px solid ${color.deep};
+  }
+`;
 
 function Lists() {
   return (
     <>
-      <div>List</div>
-      <header>
-        <Link to="borrowlist" style={{ marginRight: 10 }}>
-          빌린 목록
-        </Link>
-        <Link to="lendlist" style={{ marginRight: 10 }}>
-          빌려준 목록
-        </Link>
-        <Link to="resistlist" style={{ marginRight: 10 }}>
-          내가 쓴 글
-        </Link>
-        <Link to="likelist" style={{ marginRight: 10 }}>
-          찜한 목록
+      <header css={[container, `margin-top: ${rem(16)}`]}>
+        <Link to="/main">
+          <BackButton />
         </Link>
       </header>
+      <nav css={[container, flex]}>
+        <Link to="borrowlist" css={link}>
+          빌린 목록
+        </Link>
+        <Link to="lendlist" css={link}>
+          빌려준 목록
+        </Link>
+        <Link to="resistlist" css={link}>
+          내가 쓴 글
+        </Link>
+        <Link to="likelist" css={link}>
+          찜한 목록
+        </Link>
+      </nav>
       <Routes>
         <Route path="borrowlist" element={<BorrowList />} />
         <Route path="lendlist" element={<LendList />} />
