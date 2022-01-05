0bbe403bc133db1914c8d0f588d3d8f0ec83b88f
bgyoon <gye04056@gmail.com>
2022-01-05 17:13:53 +0900

    [Feat] #12 빌린 목록 뼈대 작업
    리스트가 없을 때 이미지 추가
    목록 탭 바 부분 색깔 변경은 미완성

--- client/src/pages/Lists/BorrowList.tsx
+++ client/src/pages/Lists/BorrowList.tsx
@@ -1,16 +1,58 @@
 import Reservation from '../../components/Reservation';
+/** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { rem } from '../../common';
+import { color, rem } from '../../common';
+import ListTab from '../../components/ListTab';
+import { Button } from '../../components/Button';
+import emptyBorrow from '../../assets/pictures/emptyBorrow.svg';
 
 const container = css`
   width: ${rem(1280)};
-  margin: ${rem(16)} auto;
+  margin: 0 auto;
+  margin-top: ${rem(36)};
+  margin-bottom: ${rem(16)};
+  text-align: center;
+`;
+
+const section = css`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20%, auto));
+  row-gap: ${rem(26)};
+`;
+
+const message = css`
+  font-size: ${rem(20)};
+  color: ${color.mid};
+  line-height: ${rem(28)};
+  margin: ${rem(20)} 0;
 `;
 
 const BorrowList = () => {
   return (
     <>
-      <Reservation />
+      <ListTab />
+      <div css={container}>
+        <img src={emptyBorrow} alt="camping" />
+        <p css={message}>
+          빌린 목록이 없어요! <br />
+          캠핑용품을 대여해서 즐거운 캠핑을 떠나보세요!
+        </p>
+        <Button
+          text="캠핑 용품 보러 가기"
+          width={`${rem(180)}`}
+          height={`${rem(43)}`}
+          background="white"
+          color={`${color.mid}`}
+          border={`1px solid ${color.mid}`}
+          size={`${rem(14)}`}
+        />
+        {/* <section css={section}>
+          <Reservation />
+          <Reservation />
+          <Reservation />
+          <Reservation />
+        </section> */}
+      </div>
     </>
   );
 };
