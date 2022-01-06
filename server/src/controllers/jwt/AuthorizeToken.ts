1c06e8ab1acb095096744392ffb79384dde9d4b3
Ohney <yhu633@gmail.com>
2022-01-06 14:46:59 +0900

    [Feat] #29 Login API 구현
    JWT generate function 구현
    Login API 구현

---
+++ server/src/controllers/jwt/AuthorizeToken.ts
@@ -0,0 +1,7 @@
+const jwt = require('jsonwebtoken');
+import { Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export {};
--- server/src/controllers/user/Login.ts
+++ server/src/controllers/user/Login.ts
@@ -1,23 +1,30 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { users } from '../../entity/users';
-import jwt from '../jwt/';
+import { generateToken } from '../jwt/GenerateToken';
 
 export default async (req: Request, res: Response) => {
   const email: string = req.body.email;
   const password: string = req.body.password;
   const usersRepository = getRepository(users);
 
-  const userInfo = await usersRepository.findOneOrFail({
-    email: email,
-    password: password,
-  });
-
-  if (userInfo) {
-    const token = jwt.generateToken(userInfo.email);
-    console.log(token);
-    return res.status(200).json({ data: userInfo });
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Bad Request' });
   } else {
-    return res.status(500).json({ message: '500' });
+    const userInfo = await usersRepository.findOneOrFail({
+      email: email,
+      password: password,
+    });
+
+    if (userInfo) {
+      const token = await generateToken(userInfo.email);
+
+      return res
+        .status(200)
+        .cookie('jwt', token, { httpOnly: true })
+        .json({ message: 'Login Successfully' });
+    } else {
+      return res.status(404).json({ message: 'User not exists' });
+    }
   }
 };
