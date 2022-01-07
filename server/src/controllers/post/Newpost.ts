26632943dc41ae40ad3e0bd21b8b7c46a6eff2c9
yechan <rhkr4080@naver.com>
2022-01-07 22:25:30 +0900

    [Feat] #106 post API
    - post 각 API token 추가
    - post PATCH API에 reviews 추가

--- server/src/controllers/post/Newpost.ts
+++ server/src/controllers/post/Newpost.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from 'express';
 import axios from 'axios';
-import dotenv from 'dotenv';
 import { getRepository } from 'typeorm';
 import { posts } from '../../entity/posts';
-dotenv.config();
+import { users } from '../../entity/users';
+import { authorizeToken } from '../jwt/AuthorizeToken';
 
 export = async (req: Request, res: Response) => {
+  interface postType {
+    category: string;
+    deposit: number;
+    rental_fee: number;
+    unavailable_dates: string[];
+    title: string;
+    content: string;
+    address: string;
+    img_urls: string[];
+  }
+
   const {
     category,
     deposit,
@@ -15,44 +26,54 @@ export = async (req: Request, res: Response) => {
     content,
     address,
     img_urls,
-  } = req.body;
-  if (
-    category === undefined ||
-    deposit === undefined ||
-    rental_fee === undefined ||
-    unavailable_dates === undefined ||
-    title === undefined ||
-    content === undefined ||
-    address === undefined ||
-    img_urls === undefined
-  ) {
-    res.status(400).json({ message: 'Bad Request' });
+  }: postType = req.body;
+
+  const decoded = await authorizeToken(req, res);
+  const userRepository = await getRepository(users);
+  const user = await userRepository.findOne({ email: decoded.email });
+
+  if (!user) {
+    res.status(401).json({ message: 'Unauthorized User' });
   } else {
-    const coordinates = await axios
-      .get(`https://dapi.kakao.com/v2/local/search/address.json`, {
-        headers: {
-          Authorization: `KakaoAK ${process.env.KAKAOREST_API}`,
-        },
-        params: {
-          query: address,
-        },
-      })
-      .then((res) => {
-        return res.data.documents[0];
+    if (
+      category === undefined ||
+      deposit === undefined ||
+      rental_fee === undefined ||
+      unavailable_dates === undefined ||
+      title === undefined ||
+      content === undefined ||
+      address === undefined ||
+      img_urls === undefined
+    ) {
+      res.status(400).json({ message: 'Bad Request' });
+    } else {
+      const coordinates = await axios
+        .get(`https://dapi.kakao.com/v2/local/search/address.json`, {
+          headers: {
+            Authorization: `KakaoAK ${process.env.KAKAOREST_API}`,
+          },
+          params: {
+            query: address,
+          },
+        })
+        .then((res) => {
+          return res.data.documents[0];
+        });
+      const postRepository = getRepository(posts);
+      await postRepository.insert({
+        category,
+        deposit,
+        rental_fee,
+        unavailable_dates,
+        title,
+        content,
+        longitude: coordinates.x,
+        latitude: coordinates.y,
+        address,
+        img_urls,
+        users_id: user,
       });
-    const postRepository = getRepository(posts);
-    await postRepository.insert({
-      category,
-      deposit,
-      rental_fee,
-      unavailable_dates,
-      title,
-      content,
-      longitude: coordinates.x,
-      latitude: coordinates.y,
-      address,
-      img_urls,
-    });
-    res.status(200).json({ message: 'Post Successfully Created' });
+      res.status(200).json({ message: 'Post Successfully Created' });
+    }
   }
 };
--- server/src/controllers/post/Post.ts
+++ server/src/controllers/post/Post.ts
@@ -1,14 +1,26 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { posts } from '../../entity/posts';
+import { users } from '../../entity/users';
+import { users_reviews } from '../../entity/users_reviews';
+import { authorizeToken } from '../jwt/AuthorizeToken';
 import axios from 'axios';
 
 export default {
   get: async (req: Request, res: Response) => {
-    const id: string = req.params.postId;
-    const postRepository = getRepository(posts);
-    const post = await postRepository.findOne({ id: Number(id) });
-    res.status(200).json({ posts: post });
+    const id: number = Number(req.params.postId);
+
+    const decoded = await authorizeToken(req, res);
+    const userRepository = await getRepository(users);
+    const user = await userRepository.findOne({ email: decoded.email });
+
+    if (!user) {
+      res.status(401).json({ message: 'Unauthorized User' });
+    } else {
+      const postRepository = getRepository(posts);
+      const post = await postRepository.findOne({ id: id });
+      res.status(200).json({ posts: post });
+    }
   },
   patch: async (req: Request, res: Response) => {
     interface postType {
@@ -21,7 +33,9 @@ export default {
       address: string;
       img_urls: string[];
     }
+
     const id: string = req.params.postId;
+
     const {
       category,
       deposit,
@@ -32,48 +46,61 @@ export default {
       address,
       img_urls,
     }: postType = req.body;
-    const postRepository = getRepository(posts);
-    if (category) {
-      await postRepository.update(Number(id), { category });
-    }
-    if (deposit) {
-      await postRepository.update(Number(id), { deposit });
-    }
-    if (rental_fee) {
-      await postRepository.update(Number(id), { rental_fee });
-    }
-    if (unavailable_dates) {
-      await postRepository.update(Number(id), { unavailable_dates });
-    }
-    if (title) {
-      await postRepository.update(Number(id), { title });
-    }
-    if (content) {
-      await postRepository.update(Number(id), { content });
-    }
-    if (img_urls) {
-      await postRepository.update(Number(id), { img_urls });
-    }
-    if (address) {
-      const coordinates = await axios
-        .get('https://dapi.kakao.com/v2/local/search/address.json', {
-          headers: {
-            Authorization: `KakaoAK ${process.env.KAKAOREST_API}`,
-          },
-          params: {
-            query: address,
-          },
-        })
-        .then((res) => {
-          return res.data.documents[0];
+
+    const decoded = await authorizeToken(req, res);
+    const userRepository = await getRepository(users);
+    const user = await userRepository.findOne({ email: decoded.email });
+
+    if (!user) {
+      res.status(401).json({ message: 'Unauthorized User' });
+    } else {
+      const postRepository = getRepository(posts);
+      const reviewRepository = getRepository(users_reviews);
+
+      if (category) {
+        await postRepository.update(Number(id), { category });
+      }
+      if (deposit) {
+        await postRepository.update(Number(id), { deposit });
+      }
+      if (rental_fee) {
+        await postRepository.update(Number(id), { rental_fee });
+      }
+      if (unavailable_dates) {
+        await postRepository.update(Number(id), { unavailable_dates });
+      }
+      if (title) {
+        await postRepository.update(Number(id), { title });
+      }
+      if (content) {
+        await postRepository.update(Number(id), { content });
+      }
+      if (img_urls) {
+        await postRepository.update(Number(id), { img_urls });
+      }
+      if (address) {
+        const coordinates = await axios
+          .get('https://dapi.kakao.com/v2/local/search/address.json', {
+            headers: {
+              Authorization: `KakaoAK ${process.env.KAKAOREST_API}`,
+            },
+            params: {
+              query: address,
+            },
+          })
+          .then((res) => {
+            return res.data.documents[0];
+          });
+        await postRepository.update(Number(id), {
+          address,
+          longitude: coordinates.x,
+          latitude: coordinates.y,
         });
-      await postRepository.update(Number(id), {
-        address,
-        longitude: coordinates.x,
-        latitude: coordinates.y,
-      });
+      }
+      const post = await postRepository.findOne({ id: Number(id) });
+      const reviews = await reviewRepository.find({ users_id: user });
+
+      res.status(200).json({ posts: post, reviews });
     }
-    const post = await postRepository.findOne({ id: Number(id) });
-    res.status(200).json({ posts: post });
   },
 };
