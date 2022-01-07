a21269ecb200437b65b14cd383bce30d47bf413d
bgyoon <gye04056@gmail.com>
2022-01-07 14:03:59 +0900

    [Style] #86 스타일 적용
    메인페이지
    - 카테고리 컴포넌트 따로 생성, 스타일 적용
    - 글쓰기 버튼 스타일 적용
    소개페이지 배경 사진 적용에 따른 변경사항
    - 각 컴포넌트 위치 스타일 적용(z-index, position)
    - 버튼 컴포넌트 배경색 props 지정
    (hover 시 투명해지면 배경사진이 보임)
    푸터 스타일 적용 및 링크 추가

--- client/src/components/ProfileDropdown.tsx
+++ client/src/components/ProfileDropdown.tsx
@@ -5,13 +5,12 @@ import { rem, shadow, textDecorationNone } from '../common';
 
 const box = css`
   width: ${rem(205)};
-  height: ${rem(284)};
   border-radius: ${rem(10)};
   position: absolute;
   top: ${rem(83)};
   right: 0;
   background-color: white;
-  z-index: 999;
+  /* z-index: 999; */
   box-shadow: ${shadow};
 `;
 
@@ -19,7 +18,7 @@ const ulStyle = css`
   list-style: none;
   padding: 0;
   font-size: ${rem(16)};
-  margin: ${rem(18)} 0;
+  margin: ${rem(18)} 0 ${rem(8)} 0;
 `;
 
 const li = css`
@@ -39,32 +38,22 @@ function ProfileDropdown() {
   return (
     <div css={box}>
       <ul css={ulStyle}>
-        <li css={li}>
-          <Link to="/lists/borrowlist" css={textDecorationNone}>
-            빌린 목록
-          </Link>
-        </li>
-        <li css={li}>
-          <Link to="/lists/lendlist" css={textDecorationNone}>
-            빌려준 목록
-          </Link>
-        </li>
-        <li css={li}>
-          <Link to="/lists/likelist" css={textDecorationNone}>
-            찜한 목록
-          </Link>
-        </li>
-        <li css={li}>
-          <Link to="/lists/resistlist" css={textDecorationNone}>
-            내가 쓴 글
-          </Link>
-        </li>
+        <Link to="/lists/borrowlist" css={textDecorationNone}>
+          <li css={li}>빌린 목록</li>
+        </Link>
+        <Link to="/lists/lendlist" css={textDecorationNone}>
+          <li css={li}>빌려준 목록</li>
+        </Link>
+        <Link to="/lists/likelist" css={textDecorationNone}>
+          <li css={li}>찜한 목록</li>
+        </Link>
+        <Link to="/lists/resistlist" css={textDecorationNone}>
+          <li css={li}>내가 쓴 글</li>
+        </Link>
         <div css={line} />
-        <li css={li}>
-          <Link to="mypage" css={textDecorationNone}>
-            계정
-          </Link>
-        </li>
+        <Link to="mypage" css={textDecorationNone}>
+          <li css={li}>계정</li>
+        </Link>
         <li css={li}>로그아웃</li>
       </ul>
     </div>
