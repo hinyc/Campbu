408a7481a3077e094032e5a098174e5c8afe3012
yechan <rhkr4080@naver.com>
2022-01-10 14:22:07 +0900

    [Docs] Entity 수정
    Entity likes, posts, reservation, users_reviews에 onDelete 설정

--- server/src/entity/likes.ts
+++ server/src/entity/likes.ts
@@ -5,6 +5,7 @@ import {
   UpdateDateColumn,
   ManyToOne,
   JoinColumn,
+  Column,
 } from 'typeorm';
 import { users } from './users';
 import { posts } from './posts';
@@ -20,14 +21,14 @@ export class likes {
   @UpdateDateColumn()
   updated_at: Date;
 
-  @ManyToOne((type) => users)
+  @ManyToOne((type) => users, (users) => users.likes, { onDelete: 'CASCADE' })
   @JoinColumn({
     name: 'users_id',
     referencedColumnName: 'id',
   })
   users_id: users;
 
-  @ManyToOne((type) => posts)
+  @ManyToOne((type) => posts, (posts) => posts.likes, { onDelete: 'CASCADE' })
   @JoinColumn({
     name: 'posts_id',
     referencedColumnName: 'id',
