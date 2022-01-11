e66e7d227e858669b14199476f61564a7e317282
bgyoon <gye04056@gmail.com>
2022-01-11 15:45:11 +0900

    [Feat] #121 카테고리 필터링 추가

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -6,8 +6,8 @@ import { rem, relative, host } from '../common';
 import SearchGreen from '../assets/SearchGreen.svg';
 import SearchInput from '../components/SearchInput';
 import Category from '../components/Category';
-import { useRecoilState } from 'recoil';
-import { posts } from '../Atom';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { posts, originalPosts } from '../Atom';
 import { useState } from 'react';
 import LoginModal from '../components/LoginModal';
 import AlertModal from '../components/AlertModal';
@@ -68,21 +68,62 @@ function Main() {
   const [showSearchModal, setShowSearchModal] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
 
+  const setMainSearch = useSetRecoilState<Posts>(originalPosts);
+
   const onChange = (e: any) => {
     setSearchValue(e.target.value);
   };
 
-  const onSearchClick = async () => {
+  const onSearchClick = () => {
     if (searchValue.length !== 0) {
       // TODO: 로딩 컴포넌트 띄우기
-      setLoading(true);
-      console.log('1st', loading);
-      console.log(showSearchModal, ' main page searching...');
-      // await axios
+      // setLoading(true);
+      // console.log('1st', loading);
+      // axios
       //   .get(`${host}/product/address/${searchValue}`)
       //   .then((res) => {
       //     if (res.status === 200) {
-      searchAddress(() => ({
+      setMainSearch({
+        // TODO: 검색 결과를 여기에 추가
+        // posts: [],
+        posts: [
+          {
+            id: 1,
+            category: 'Etc',
+            deposit: 30000,
+            rental_fee: 25000,
+            unavailable_dates: ['2021-12-20', '2021-12-21', '2021-12-22'],
+            title: '검색했을 때 새로 나오는거',
+            content: '쉽게 설치할 수 있는 3~4인용 텐트입니다.',
+            longitude: 126.99597295767953,
+            latitude: 35.97664845766847,
+            address: '서울특별시 동작구 신대방동',
+            img_urls:
+              'https://paperbarkcamp.com.au/wp-content/uploads/2019/07/paperbark_flash-camp_news_1218x650.jpg',
+            users_id: 1,
+            reservation_dates: ['2021-12-29', '2021-12-30', '2021-12-31'],
+            likes_count: 15,
+          },
+          {
+            id: 1,
+            category: 'Pot',
+            deposit: 30000,
+            rental_fee: 25000,
+            unavailable_dates: ['2021-12-20', '2021-12-21', '2021-12-22'],
+            title: '메인페이지 검색',
+            content: '쉽게 설치할 수 있는 3~4인용 텐트입니다.',
+            longitude: 126.99597295767953,
+            latitude: 35.97664845766847,
+            address: '서울특별시 동작구 신대방동',
+            img_urls:
+              'https://paperbarkcamp.com.au/wp-content/uploads/2019/07/paperbark_flash-camp_news_1218x650.jpg',
+            users_id: 1,
+            reservation_dates: ['2021-12-29', '2021-12-30', '2021-12-31'],
+            likes_count: 15,
+          },
+        ],
+      });
+      searchAddress({
         // TODO: 검색 결과를 여기에 추가
         // posts: [],
         posts: [
@@ -121,13 +162,13 @@ function Main() {
             likes_count: 15,
           },
         ],
-      }));
+      });
       //   }
       // })
       // .catch((err) => console.error(err));
       // TODO: 로딩 컴포넌트
-      setLoading(false);
-      console.log('2nd', loading);
+      // setLoading(false);
+      // console.log('2nd', loading);
     } else {
       console.log('input text please');
       // TODO: false로 초기화 시키기
@@ -169,9 +210,10 @@ function Main() {
         </div>
       ) : (
         <section css={section}>
-          {products['posts'].map((product: Post) => (
+          {products['posts'].map((product: Post, index) => (
             <Product
-              setModalShow={setLoginModalShow}
+              // setModalShow={setLoginModalShow}
+              key={index}
               count={product.likes_count}
               // TODO: 좋아요 눌렀는지 안눌렀는지 상태 변경
               isFill={false}
