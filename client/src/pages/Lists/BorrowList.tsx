f9a822b0e10a0bc33e7a58aa698f4bda4b9ad33f
bgyoon <gye04056@gmail.com>
2022-01-12 23:51:13 +0900

    [Feat] #117 예약 상테에 따른 확인 모달 적용
    예약 상테에 따라 확인 모달 띄우게 map 함수 안에 두었는데, 버튼을 클릭하면 모달에 필요한 인자를 보내주어 예약 상태에 맞는 모달을 띄우도록 수정

--- client/src/pages/Lists/BorrowList.tsx
+++ client/src/pages/Lists/BorrowList.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import Reservation from '../../components/Reservation';
 import { css } from '@emotion/react';
-import { color, rem, flex, textDecorationNone } from '../../common';
+import { color, rem, flex, textDecorationNone, host } from '../../common';
 import ListTab from '../../components/ListTab';
 import { Link } from 'react-router-dom';
 import { link, visit } from './tab';
@@ -10,8 +10,9 @@ import emptyBorrow from '../../assets/pictures/emptyBorrow.svg';
 import { container, section, message } from './tab';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { borrows, showConfirmModal, UserPost } from '../../Atom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import YesOrNo from '../../components/YesOrNo';
+import axios from 'axios';
 
 interface Borrow {
   reservation: List[];
@@ -28,20 +29,28 @@ interface List {
 
 function BorrowList() {
   const [borrowLists, setBorrowLists] = useRecoilState<Borrow>(borrows);
-  const [buttonClick, setButtonClick] = useRecoilState(showConfirmModal);
+  const [confirm, setConfirm] = useRecoilState(showConfirmModal);
   const button = ['예약 취소', '반납하기', '반납 확인 대기 중', '반납 완료'];
-  const body1 = [`예약을 취소하시겠습니까?`, `반납하시겠습니까?`];
-  const body2 = [
-    `대여자가 예약을 수락하기 전까지 취소할 수 있습니다.`,
-    `대여자가 상품 회수 후 반납 확인 시 최종 반납 처리가 됩니다.`,
-  ];
+  const [reservationId, setReservationId] = useState(0);
+  const [reservationStatus, setReservationStatus] = useState(0);
   const printStatusText = (status: number) => button[status - 1];
-  const printBody1Text = (status: number) => body1[status - 1];
-  const printBody2Text = (status: number) => body2[status - 1];
 
-  const onButtonClick = () => {
-    setButtonClick(true);
+  const onButtonClick = (id: number, status: number) => {
+    setReservationId(id);
+    setReservationStatus(status);
+    setConfirm(true);
   };
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`${host}/userinfo/product/borrow`)
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       setBorrowLists(res.data);
+  //     })
+  //     .catch((err) => console.error(err));
+  // });
+
   return (
     <>
       <ListTab />
@@ -60,15 +69,27 @@ function BorrowList() {
         </Link>
       </nav>
       <div css={container}>
-        {/* {buttonClick && (
-          <YesOrNo
-            //! reservation_status에 따라 버튼 text 바꾸기
-            text="취소"
-            title="예약 취소"
-            text1="예약을 취소하시겠습니까?"
-            text2="대여자가 예약을 수락하기 전까지 취소할 수 있습니다."
-          />
-        )} */}
+        {confirm &&
+          ((reservationStatus === 1 && (
+            <YesOrNo
+              reservationId={reservationId}
+              reservation_status={1}
+              text={'예약 취소'}
+              title={'예약 취소'}
+              text1={`예약을 취소하시겠습니까?`}
+              text2={`대여자가 예약을 수락하기 전까지 취소할 수 있습니다.`}
+            />
+          )) ||
+            (reservationStatus === 2 && (
+              <YesOrNo
+                reservationId={reservationId}
+                reservation_status={2}
+                text={'반납하기'}
+                title={'반납하기'}
+                text1={`반납하시겠습니까?`}
+                text2={`대여자가 상품 회수 후 반납 확인 시 최종 반납 처리가 됩니다.`}
+              />
+            )))}
         {borrowLists['reservation'].length === 0 ? (
           <>
             <img src={emptyBorrow} alt="camping" />
@@ -92,52 +113,40 @@ function BorrowList() {
           <section css={section}>
             {borrowLists['reservation'].map(
               (borrowList: List, index: number) => (
-                <>
-                  <Reservation
-                    key={index}
-                    text={printStatusText(borrowList.reservation_status)}
-                    background={
-                      borrowList.reservation_status !== 4
-                        ? `${color.point}`
-                        : `${color.mid}`
-                    }
-                    color="white"
-                    cursor={
-                      borrowList.reservation_status === 1 ||
-                      borrowList.reservation_status === 2
-                        ? 'pointer'
-                        : 'not-allowed'
-                    }
-                    hover={
-                      borrowList.reservation_status === 4
-                        ? '100%'
-                        : borrowList.reservation_status === 3
-                        ? '50%'
-                        : '80%'
-                    }
-                    opacity={
-                      borrowList.reservation_status === 3 ? '50%' : '100%'
-                    }
-                    postId={borrowList.posts.id}
-                    img_urls={borrowList.posts.img_urls}
-                    address={borrowList.posts.address}
-                    title={borrowList.posts.title}
-                    deposit={borrowList.posts.deposit}
-                    rental_fee={borrowList.posts.rental_fee}
-                    reservation_dates={borrowList.reservation_dates}
-                    onButtonClick={onButtonClick}
-                  />
-                  {buttonClick && (
-                    <YesOrNo
-                      reservationId={borrowList.id}
-                      reservation_status={borrowList.reservation_status}
-                      text={printStatusText(borrowList.reservation_status)}
-                      title={printStatusText(borrowList.reservation_status)}
-                      text1={printBody1Text(borrowList.reservation_status)}
-                      text2={printBody2Text(borrowList.reservation_status)}
-                    />
-                  )}
-                </>
+                <Reservation
+                  key={index}
+                  text={printStatusText(borrowList.reservation_status)}
+                  background={
+                    borrowList.reservation_status !== 4
+                      ? `${color.point}`
+                      : `${color.mid}`
+                  }
+                  color="white"
+                  cursor={
+                    borrowList.reservation_status === 1 ||
+                    borrowList.reservation_status === 2
+                      ? 'pointer'
+                      : 'not-allowed'
+                  }
+                  hover={
+                    borrowList.reservation_status === 4
+                      ? '100%'
+                      : borrowList.reservation_status === 3
+                      ? '50%'
+                      : '80%'
+                  }
+                  opacity={borrowList.reservation_status === 3 ? '50%' : '100%'}
+                  postId={borrowList.posts.id}
+                  img_urls={borrowList.posts.img_urls}
+                  address={borrowList.posts.address}
+                  title={borrowList.posts.title}
+                  deposit={borrowList.posts.deposit}
+                  rental_fee={borrowList.posts.rental_fee}
+                  reservation_dates={borrowList.reservation_dates}
+                  onButtonClick={() =>
+                    onButtonClick(borrowList.id, borrowList.reservation_status)
+                  }
+                />
               ),
             )}
           </section>
