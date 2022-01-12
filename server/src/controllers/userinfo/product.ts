f110ca0d3a5ab739aca3436759e670c5ef1a9f97
Henryy <yhu633@gmail.com>
2022-01-12 09:54:43 +0900

    [Feat] Userinfo product API 구현(borrow, lend)
    Borrow, lend 라우터연결
    Borrow, lend 기능 구현

--- server/src/controllers/userinfo/product.ts
+++ server/src/controllers/userinfo/product.ts
@@ -1,16 +1,14 @@
 import { Request, Response } from 'express';
 import { createQueryBuilder, getRepository } from 'typeorm';
 import { users } from '../../entity/users';
+import { posts } from '../../entity/posts';
 import { reservation } from '../../entity/reservation';
 import { authorizeToken } from '../jwt/AuthorizeToken';
-import { posts } from '../../entity/posts';
 
 export default {
   borrow: async (req: Request, res: Response) => {
     const decoded = await authorizeToken(req, res);
     const usersRepository = getRepository(users);
-    const reserveRepository = getRepository(reservation);
-    const postRepository = getRepository(posts);
 
     const userInfo = await usersRepository.findOne({
       email: decoded.email,
@@ -20,10 +18,36 @@ export default {
       return res.status(401).json({ message: 'Unauthorized User' });
     } else {
       const reservationInfo = await createQueryBuilder('reservation')
-        .leftJoinAndSelect(posts, 'posts', 'reservation.posts_id = posts.id')
+        .leftJoinAndSelect(
+          posts,
+          'posts',
+          'reservation.users_id = posts.users_id',
+        )
         .getMany();
 
-      res.status(200).json({ reservation: reservationInfo });
+      return res.status(200).json({ reservation: reservationInfo });
+    }
+  },
+  lend: async (req: Request, res: Response) => {
+    const decoded = await authorizeToken(req, res);
+    const usersRepository = getRepository(users);
+
+    const userInfo = await usersRepository.findOne({
+      email: decoded.email,
+    });
+
+    if (!userInfo) {
+      return res.status(401).json({ message: 'Unauthorized User' });
+    } else {
+      const postsInfo = await createQueryBuilder('posts')
+        .leftJoinAndSelect(
+          reservation,
+          'reservation',
+          'reservation.posts_id = posts.id',
+        )
+        .getRawMany();
+
+      return res.status(200).json({ posts: postsInfo });
     }
   },
 };
--- server/src/routes/Userinfo.ts
+++ server/src/routes/Userinfo.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/account', accountController.get);
 router.get('/product/borrow', productController.borrow);
-router.get('/product/lend');
+router.get('/product/lend', productController.lend);
 router.get('/product/likes');
 router.get('/product/post');
 router.patch('/account', accountController.patch);
